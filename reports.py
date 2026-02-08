f75afa29c52e93b05c7c46fc23022e82203b0d06
anor23 <burgchiken@gmail.com>
2026-02-08 14:17:16 +0300

    улучшен код добавлена обработка ошибок и нормализация путей

--- reports.py
+++ reports.py
@@ -1,8 +1,9 @@
 import csv
 
+from utils import path_normalizer
+
 
 class Report:
-    name = ''
 
     def __init__(self, name='', headers=None, rows=None):
         self.name = name
@@ -11,24 +12,37 @@ class Report:
 
     def read_files(self, paths):
         rows = []
-        for path in paths:
-            with open(path, 'r', encoding='utf-8') as file:
-                reader = csv.reader(file)
-                headers = next(reader)
-                file_data = list(reader)
-                rows.extend(file_data)
 
+        for path in paths:
+            path =  path_normalizer(path)
+            if not path:
+                continue
+            try:
+                with open(path, 'r', encoding='utf-8') as file:
+                    reader = csv.reader(file)
+                    headers = next(reader)
+                    file_data = list(reader)
+                    rows.extend(file_data)
+            except FileNotFoundError:
+                print('Ошибка Неверный путь к файлу или не его не существует',path)
+                continue
+            except Exception:
+                print('Ошибка чтения файла')
+                continue
             self.headers = headers
             self.rows = rows
 
     def sort_report(self, ind_key, reverse=False):
-
+        if not self.rows:
+            return 'не созданы строки таблицы'
         rows = sorted(self.rows, key=lambda row: row[ind_key], reverse=reverse)
         self.rows = rows
 
     def create_report_gdp(self):
         report = {}
         count = {}
+        if not self.headers or not  self.rows:
+            return 'не созданы строки таблицы или заголовки'
         for i in range(len(self.rows)):
             country = self.rows[i][0]
             gdp = int(self.rows[i][2])
---
+++ utils.py
@@ -0,0 +1,21 @@
+import os
+def path_normalizer(filepath:str):
+    filepath = filepath.strip()
+    if not filepath:
+        return False
+    if  filepath  in ('.', '..', '/','\\'):
+        return False
+
+
+    if not filepath.endswith('.csv') :
+        return False
+
+    if filepath.count('.') == 0:
+        filepath+= '.csv'
+
+    filepath = os.path.normpath(filepath)
+
+    return filepath
+
+
+pat = path_normalizer("D:\\data\\reports\\file",)
